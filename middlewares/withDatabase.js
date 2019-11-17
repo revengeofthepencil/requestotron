315e88d51003c9568e454699f0b7ac6fecd67112
awalker <revengeofthepencil@gmail.com>
2019-11-17 15:14:38 -0800

    re-name project

--- middlewares/withDatabase.js
+++ middlewares/withDatabase.js
@@ -6,11 +6,11 @@ const client = new MongoClient(mongoURI, { useNewUrlParser: true });
 const withDatabase = handler => (req, res) => {
     if (!client.isConnected()) {
         return client.connect().then(() => {
-            req.db = client.db('reportotron');
+            req.db = client.db('requestotron');
             return handler(req, res);
         });
     }
-    req.db = client.db('reportotron');
+    req.db = client.db('requestotron');
     return handler(req, res);
 };
 export default withDatabase;
